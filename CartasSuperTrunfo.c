e2e98c969a7b70c70ad242ae643a274cbfd35d27
wilsonrogerio <108371734+wilsonrogerio@users.noreply.github.com>
2025-01-30 12:26:15 +0000

    adicionado comentarios de orientacao no codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,15 +7,13 @@
 //Teste larissa
 
 int main() {
-
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+//variaveis que irao armazenar os dados pedidos
     char codigo[4];
     int populacao;
     float area;
     float pib;
     int pontos_turisticos;
-
+//entrada de dados
     puts("Digite o codigo da Cidade ex A01: ");
     scanf("%s", codigo);
     printf("Digite a populacao da Cidade\n");
@@ -26,6 +24,7 @@ int main() {
     scanf("%f", &pib);
     printf("Digite o numero de pontos turisticos da Cidade\n");
     scanf("%d", &pontos_turisticos);
+    //saida de dados com formatacao
     printf("Codigo da Cidade: %s\n", codigo);
     printf("Populacao da Cidade: %.3d\n", populacao);
     printf("Área: %.3f km²\n", area);
