0c255e4d5d41ccfe676acc173d9b067b3d7f303a
wilsonrogerio <108371734+wilsonrogerio@users.noreply.github.com>
2025-01-30 12:13:58 +0000

    criacao do nivel novato super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,29 @@
 //Teste larissa
 
 int main() {
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char codigo[4];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
 
+    puts("Digite o codigo da Cidade ex A01: ");
+    scanf("%s", codigo);
+    printf("Digite a populacao da Cidade\n");
+    scanf("%d", &populacao);
+    printf("Digite a area da Cidade\n");
+    scanf("%f", &area);
+    printf("Digite o PIB da Cidade\n");
+    scanf("%f", &pib);
+    printf("Digite o numero de pontos turisticos da Cidade\n");
+    scanf("%d", &pontos_turisticos);
+    printf("Codigo da Cidade: %s\n", codigo);
+    printf("Populacao da Cidade: %.3d\n", populacao);
+    printf("Área: %.3f km²\n", area);
+    printf("PIB da Cidade: %.2f\n", pib);
+    printf("Pontos turisticos da Cidade: %d\n", pontos_turisticos);
     return 0;
 }
