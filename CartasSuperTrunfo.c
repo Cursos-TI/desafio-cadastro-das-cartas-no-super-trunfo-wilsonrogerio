fbc4e801e8be86960a1d63ce7c7c25a1d652526d
wilsonrogerio <108371734+wilsonrogerio@users.noreply.github.com>
2025-02-23 13:09:07 +0000

    finalizacao do jogo, todas as funcionalidades adicionadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-//classe que ira armazenar a base de cada cidade
+// Definindo a estrutura para os dados da cidade
 typedef struct {
     char codigo[4];
     int populacao;
@@ -17,11 +12,23 @@ typedef struct {
     float superPoder;
 } Cidade;
 
-// Funcao para calcular o superPoder de cada cidade
+// Função para calcular o superPoder
 float calcularSuperPoder(Cidade cidade) {
     return (cidade.populacao + cidade.area + cidade.pib + cidade.pontos_turisticos + cidade.densidade_populacional + cidade.pib_per_capita) / 6.0;
 }
-// funcao aonde os dados serao coletados para cada cidade
+
+// Função para comparar os valores de superPoder de duas cidades
+void comparar_cidades(Cidade cidade1, Cidade cidade2) {
+    if (cidade1.superPoder > cidade2.superPoder) {
+        printf("A cidade com o maior superPoder é: %s\n", cidade1.codigo);
+    } else if (cidade1.superPoder < cidade2.superPoder) {
+        printf("A cidade com o maior superPoder é: %s\n", cidade2.codigo);
+    } else {
+        printf("As cidades %s e %s têm o mesmo superPoder.\n", cidade1.codigo, cidade2.codigo);
+    }
+}
+
+// Função para cadastrar os dados da cidade
 void cadastrar_cidade(Cidade* cidade) {
     printf("Código da cidade (ex: A01): ");
     scanf("%s", cidade->codigo);
@@ -33,21 +40,31 @@ void cadastrar_cidade(Cidade* cidade) {
     scanf("%f", &cidade->pib);
     printf("Número de pontos turísticos: ");
     scanf("%d", &cidade->pontos_turisticos);
+
+    // Calcula as propriedades adicionais
+    cidade->densidade_populacional = cidade->populacao / cidade->area;
+    cidade->pib_per_capita = cidade->pib / cidade->populacao;
+    cidade->superPoder = calcularSuperPoder(*cidade);
 }
 
-void exibirCidade(Cidade cidades){
-    printf("\nCódigo: %s\n", cidades.codigo);
-    printf("População: %d\n", cidades.populacao);
+// Função para exibir os dados da cidade
+void exibir_cidade(Cidade cidade) {
+    printf("\nCódigo: %s\n", cidade.codigo);
+    printf("População: %d\n", cidade.populacao);
+    printf("Área: %.2f km²\n", cidade.area);
+    printf("PIB: %.2f bilhões\n", cidade.pib);
+    printf("Número de pontos turísticos: %d\n", cidade.pontos_turisticos);
+    printf("Densidade Populacional: %.2f hab/km²\n", cidade.densidade_populacional);
+    printf("PIB per Capita: %.2f bilhões/hab\n", cidade.pib_per_capita);
+    printf("Super Poder: %.2f\n", cidade.superPoder);
 }
 
-
+// Método principal que irá chamar as funções
 int main() {
-//variaveis que irao armazenar os dados pedidos
-    Cidade cidades[32]; // limitando o numero de cidades em 32 apenas
-    int cidades_total = 0, opcao;
-//entrada de dados
-    do
-    {
+    Cidade cidades[32];  // Limite de 32 cidades
+    int total_cidades = 0, opcao;
+
+    do {
         printf("\n1. Cadastrar cidade\n");
         printf("2. Exibir cidades\n");
         printf("3. Comparar superPoder entre duas cidades\n");
@@ -55,64 +72,44 @@ int main() {
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
 
-        switch (opcao)
-        {
-        case 1 :
-            if (cidades < 32)
-            {
-                cadastrar_cidade(&cidades[cidades_total]);
-                cidades_total++;
-            }else{
-                printf("Limite de 32 cidades foi atingido");
-            }
-            
-            break;
-        case 2:
-            for ( int i = 0; i < cidades_total; i++)
-            {
-               exibirCidade(cidades[i]);
-            }
-            
-
-
-        break;
-
-        case 3:
-
-
-        break;
-
-
-        case 0 :
-
-
-        break;
-        
-        default:
-            break;
+        switch (opcao) {
+            case 1:
+                if (total_cidades < 32) {
+                    cadastrar_cidade(&cidades[total_cidades]);
+                    total_cidades++;
+                } else {
+                    printf("Limite de cidades cadastradas atingido.\n");
+                }
+                break;
+            case 2:
+                for (int i = 0; i < total_cidades; i++) {
+                    exibir_cidade(cidades[i]);
+                }
+                break;
+            case 3:
+                if (total_cidades >= 2) {
+                    int cidade1, cidade2;
+                    printf("Digite o índice da primeira cidade (0 a %d): ", total_cidades - 1);
+                    scanf("%d", &cidade1);
+                    printf("Digite o índice da segunda cidade (0 a %d): ", total_cidades - 1);
+                    scanf("%d", &cidade2);
+                    if (cidade1 >= 0 && cidade1 < total_cidades && cidade2 >= 0 && cidade2 < total_cidades) {
+                        comparar_cidades(cidades[cidade1], cidades[cidade2]);
+                    } else {
+                        printf("Índices de cidades inválidos.\n");
+                    }
+                } else {
+                    printf("É necessário cadastrar pelo menos 2 cidades para comparar.\n");
+                }
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida.\n");
+                break;
         }
-        /* code */
     } while (opcao != 0);
-    
 
-
-
-
-    // puts("Digite o codigo da Cidade ex A01: ");
-    // scanf("%s", codigo);
-    // printf("Digite a populacao da Cidade\n");
-    // scanf("%d", &populacao);
-    // printf("Digite a area da Cidade\n");
-    // scanf("%f", &area);
-    // printf("Digite o PIB da Cidade\n");
-    // scanf("%f", &pib);
-    // printf("Digite o numero de pontos turisticos da Cidade\n");
-    // scanf("%d", &pontos_turisticos);
-    // //saida de dados com formatacao
-    // printf("Codigo da Cidade: %s\n", codigo);
-    // printf("Populacao da Cidade: %.3d\n", populacao);
-    // printf("Área: %.3f km²\n", area);
-    // printf("PIB da Cidade: %.2f\n", pib);
-    // printf("Pontos turisticos da Cidade: %d\n", pontos_turisticos);
-    // return 0;
+    return 0;
 }
